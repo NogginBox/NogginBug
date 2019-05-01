d6b47e02a2638fafefd9b64fb8c6f8bafe930fd8
Richard Garside <richard.garside@nogginbox.co.uk>
2019-05-01 17:38:20 +0100

    Added SpecFlow Feature ViewOpenBugs

---
+++ NogginBug.Tests/Features/Steps/ViewOpenBugsSteps.cs
@@ -0,0 +1,44 @@
+﻿using Coypu;
+using Coypu.Drivers.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using TechTalk.SpecFlow;
+
+namespace NogginBug.Tests.Features.Steps
+{
+    [Binding]
+    public class ViewOpenBugsSteps
+    {
+        private int _expectedBugsWithOpenStatus;
+
+        [Given(@"there are (.*) bugs with status open")]
+        public void GivenThereAreBugsWithStatusOpen(int numOpenBugs)
+        {
+            _expectedBugsWithOpenStatus = numOpenBugs;
+        }
+        
+        [Then(@"I see a list of the titles of all bugs with open status")]
+        public void ThenISeeAListOfTheTitlesOfAllBugsWithOpenStatus()
+        {
+            var config = new SessionConfiguration
+            {
+                AppHost = "localhost",
+                Port = 44314,
+                SSL = true,
+                //Driver = typeof(SeleniumWebDriver),
+                //Browser = Coypu.Drivers.Browser.Chrome
+                
+            };
+
+            var driver = new SeleniumWebDriver(Coypu.Drivers.Browser.Chrome);
+            driver.
+
+            using (var browser = new BrowserSession(config, driver))
+            {
+                //var ff = browser.Native as FirefoxDriver;
+
+                browser.Visit("/");
+            }
+            }
+    }
+}
---
+++ NogginBug.Tests/Features/ViewOpenBugs.feature
@@ -0,0 +1,10 @@
+﻿Feature: View Open Bugs
+	In order to see current bugs
+	As a user
+	I want to see the titles of all bugs with open status
+
+Scenario: Visit home page to see open bugs
+       Given there are 2 bugs with status open
+       # And I have also entered 70 into the calculator
+       # When I press add
+       Then I see a list of the titles of all bugs with open status
